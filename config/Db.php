d8986fc4c8c8d6a3bae01be39c72e24bc1f37f12
Xennis <code@xennis.org>
2015-03-21 21:26:11 +0100

    Update creation of tables

--- config/Db.php
+++ config/Db.php
@@ -1,8 +1,16 @@
 <?php
 abstract class MT_Config_Db {
 	
+	private static function createTable($tableName, $sql) {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();		
+		
+		$sql = "CREATE TABLE IF NOT EXISTS `".$wpdb->prefix."mt_$tableName` ($sql) $charset_collate;";	
+		$wpdb->query($sql);
+	}
+	
 	public static function __setup_database_tables() {
-		MT_Common::__createTable('category', "
+		self::createTable('category', "
 			`id` tinyint(2) unsigned NOT NULL AUTO_INCREMENT,
 			`name` varchar(25) NOT NULL,
 			`path` varchar(30) NOT NULL,
@@ -10,7 +18,7 @@ abstract class MT_Config_Db {
 			PRIMARY KEY (`id`),
 			UNIQUE KEY `path` (`path`)		
 		");
-		MT_Common::__createTable('gallery', "
+		self::createTable('gallery', "
 			`id` tinyint(2) unsigned NOT NULL AUTO_INCREMENT,
 			`category` tinyint(2) unsigned NOT NULL,
 			`subcategory` tinyint(2) unsigned NOT NULL DEFAULT '0',
@@ -24,14 +32,14 @@ abstract class MT_Config_Db {
 			PRIMARY KEY (`id`),
 			UNIQUE KEY `fullPath` (`fullPath`)		
 		");	
-		MT_Common::__createTable('management_temp', "
+		self::createTable('management_temp', "
 			`ip` varchar(15) NOT NULL DEFAULT '0',
 			`num` tinyint(2) unsigned NOT NULL DEFAULT '10',
 			`sort` varchar(5) NOT NULL DEFAULT 'date',
 			`date` int(10) unsigned NOT NULL,
 			PRIMARY KEY (`ip`)		
 		");
-		MT_Common::__createTable('news', "
+		self::createTable('news', "
 			`id` smallint(3) unsigned NOT NULL AUTO_INCREMENT,
 			`title` varchar(100) NOT NULL,
 			`text` text NOT NULL,
@@ -39,7 +47,7 @@ abstract class MT_Config_Db {
 			`date` int(10) unsigned NOT NULL,
 			PRIMARY KEY (`id`)		
 		");
-		MT_Common::__createTable('photo', "
+		self::createTable('photo', "
 			`id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
 			`path` varchar(100) NOT NULL,
 			`name_old` varchar(30) NOT NULL,
@@ -51,7 +59,7 @@ abstract class MT_Config_Db {
 			PRIMARY KEY (`id`),
 			UNIQUE KEY `photo_path` (`path`)	
 		");
-		MT_Common::__createTable('photographer', "
+		self::createTable('photographer', "
 			`id` tinyint(2) unsigned NOT NULL AUTO_INCREMENT,
 			`name` varchar(40) NOT NULL,
 			`camera` varchar(20) NOT NULL,
@@ -59,7 +67,7 @@ abstract class MT_Config_Db {
 			PRIMARY KEY (`id`),
 			UNIQUE KEY `name` (`name`)
 		");
-		MT_Common::__createTable('subcategory', "
+		self::createTable('subcategory', "
 			`id` tinyint(2) unsigned NOT NULL AUTO_INCREMENT,
 			`category` tinyint(2) unsigned NOT NULL,
 			`name` varchar(30) NOT NULL,
