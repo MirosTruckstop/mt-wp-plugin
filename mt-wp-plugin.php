dd23cf00dc07fa0c2b01fa25df686961c803f580
Xennis <code@xennis.org>
2015-05-19 01:13:53 +0200

    Remove options page

--- mt-wp-plugin.php
+++ mt-wp-plugin.php
@@ -320,7 +320,7 @@ add_action('admin_menu', 'mt_admin_menu');
 function mt_admin_menu() {
 
     // Add a new submenu under Settings:
-    add_options_page(__('Test Settings','menu-test'), __('Test Settings','menu-test'), 'manage_options', 'testsettings', 'mt_settings_page');
+    //add_options_page(__('Test Settings','menu-test'), __('Test Settings','menu-test'), 'manage_options', 'testsettings', 'mt_settings_page');
 
     // Add a new submenu under Tools:
 	//add_management_page( __('Test Tools','menu-test'), __('Test Tools','menu-test'), 'manage_options', 'testtools', 'mt_tools_page');
@@ -338,70 +338,6 @@ function mt_admin_menu() {
     add_submenu_page('mt-news', 'Galerien', 'Galerien', 'manage_options', 'mt-gallery', 'mt_page_galleries');
 	add_submenu_page('mt-news', 'Fotografen', 'Fotografen', 'manage_options', 'mt-photographer', 'mt_page_photographers');
 	add_submenu_page('mt-news', 'Vorschaubilder', 'Vorschaubilder', 'manage_options', 'mt-thumbnail', 'mt_page_thumbnails');
-}
-
-// mt_settings_page() displays the page content for the Test settings submenu
-function mt_settings_page() {
-
-    //must check that the user has the required capability 
-    if (!current_user_can('manage_options'))
-    {
-      wp_die( __('You do not have sufficient permissions to access this page.') );
-    }
-
-    // variables for the field and option names 
-    $opt_name = 'mt_favorite_color';
-    $hidden_field_name = 'mt_submit_hidden';
-    $data_field_name = 'mt_favorite_color';
-
-    // Read in existing option value from database
-    $opt_val = get_option( $opt_name );
-
-    // See if the user has posted us some information
-    // If they did, this hidden field will be set to 'Y'
-    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
-        // Read their posted value
-        $opt_val = $_POST[ $data_field_name ];
-
-        // Save the posted value in the database
-        update_option( $opt_name, $opt_val );
-
-        // Put an settings updated message on the screen
-
-?>
-<div class="updated"><p><strong><?php _e('settings saved.', 'menu-test' ); ?></strong></p></div>
-<?php
-
-    }
-
-    // Now display the settings editing screen
-
-    echo '<div class="wrap">';
-
-    // header
-
-    echo "<h2>" . __( 'Menu Test Plugin Settings', 'menu-test' ) . "</h2>";
-
-    // settings form
-    
-    ?>
-
-<form name="form1" method="post" action="">
-<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
-
-<p><?php _e("Favorite Color:", 'menu-test' ); ?> 
-<input type="text" name="<?php echo $data_field_name; ?>" value="<?php echo $opt_val; ?>" size="20">
-</p><hr />
-
-<p class="submit">
-<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
-</p>
-
-</form>
-</div>
-
-<?php
- 
 }
 
 function mt_page_photos() {
